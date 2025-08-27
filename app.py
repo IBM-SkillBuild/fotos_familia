455389ff5cc26da51854aae31435f7a0e5b31add
IBM-SkillBuild <Api.Nasa.2022@gmail.com>
2025-08-27 20:59:47 +0200

    003

--- app.py
+++ app.py
@@ -4560,4 +4560,4 @@ def robots_txt():
 if __name__ == '__main__':
     init_db()
     app_logger.info("APLICACION INICIADA")
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=True, host='0.0.0.0', port=5004)
