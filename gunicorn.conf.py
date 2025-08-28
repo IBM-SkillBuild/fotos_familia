f024234a8d5f684becde37aa682156b6fb8181ff
IBM-SkillBuild <Api.Nasa.2022@gmail.com>
2025-08-28 11:35:28 +0200

    ok

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,7 @@ import multiprocessing
 
 # Número de workers
 workers = multiprocessing.cpu_count() * 2 + 1
+threads = 4 # Añadido para manejar más peticiones concurrentes
 
 # Timeouts
 timeout = 120  # 120 segundos
